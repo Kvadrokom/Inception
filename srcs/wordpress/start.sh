a16c7d619833d4fb270383444824e146d1789f64
skharjo <skharjo@student.21-school.ru>
2022-02-25 20:53:38 +0300

    srcs/mariadb/start.sh

--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -1,4 +1,4 @@
-if ! [ -f "/var/www/wordpress/wp-config.php" ]; then
+if ! [ -d "/var/www/wordpress/wp-config.php" ]; then
     wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --allow-root
     wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${USER_NAME} --admin_password=${MYSQL_ROOT_PASSWORD} --admin_email=${USER_ADMIN_EMAIL} --skip-email --allow-root
     wp user create ${MYSQL_USER} ${USER_EMAIL} --allow-root --role=subscriber --user_pass=${MYSQL_PASSWORD}
