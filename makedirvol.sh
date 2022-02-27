d1470afb4e91d0283c96e51697f1819285630760
skharjo <skharjo@student.21-school.ru>
2022-02-27 15:59:15 +0300

    sc

---
+++ makedirvol.sh
@@ -0,0 +1,7 @@
+if ! [ -d "/home/skharjo/data/wordpress" ]; then
+sudo mkdir /home/skharjo/data/wordpress
+fi
+
+if ! [ -d "/home/skharjo/data/mariadb" ]; then
+sudo mkdir /home/skharjo/data/mariadb
+fi
