89410d241dbf8e31fc77d12cac6a9422db821d6c
Kvadrokom <63579559+Kvadrokom@users.noreply.github.com>
2022-03-06 14:37:30 +0300

    add sleep

--- srcs/mariadb/start.sh
+++ srcs/mariadb/start.sh
@@ -1,6 +1,7 @@
 chown -R mysql:mysql /var/lib/mysql
 if ! [ -d "/var/lib/mysql/wordpress" ]; then
     service mysql start
+    sleep 10
     mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARACTER SET utf8;"
     mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%';"
