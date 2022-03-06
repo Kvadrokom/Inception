4c5aa50e665614219a061a8e2cee922b471dd09f
Kvadrokom <63579559+Kvadrokom@users.noreply.github.com>
2022-03-06 18:33:36 +0300

    add sleep

--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -1,4 +1,5 @@
 if ! [ -f "/var/www/wordpress/wp-config.php" ]; then
+    sleep 10
     wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --allow-root
     wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${DB_USER_ADMIN} --admin_password=${DBUSER_ADMIN_PASS} --admin_email=${USER_ADMIN_EMAIL} --skip-email --allow-root
     wp user create ${MYSQL_USER} ${USER_EMAIL} --allow-root --role=subscriber --user_pass=${MYSQL_PASSWORD}
